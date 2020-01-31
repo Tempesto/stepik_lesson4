9119fb8cd53faffe6dd0a7deca66476675360bd6
Tempesto <igor_it_step@gmail.com>
2020-01-31 10:46:25 +0200

    Add to test_main_page.py test is element presented

--- test_main_page.py
+++ test_main_page.py
@@ -4,6 +4,7 @@ from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 from .pages.locators.base_page_locators import BasePageLocators
 from .pages.locators.main_page_locator import MainPageLocators
+from .pages.locators.product_page_locator import ProductPageLocator
 
 link = 'http://selenium1py.pythonanywhere.com/'
 link_product = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
@@ -30,6 +31,7 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.check_the_presence_of_the_inscription_empty_basket()
     text_value = basket_page.text_basket_empty()
+    basket_page.is_element_present(*ProductPageLocator.GET_ALERT_ABOUT_PROTUCT_ADD_TO_BASKET)
     assert "Your basket is empty." in text_value, "Text not present in basket"
 
 
@@ -40,4 +42,5 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.check_the_presence_of_the_inscription_empty_basket()
     text_value = basket_page.text_basket_empty()
+    basket_page.is_element_present(*ProductPageLocator.GET_ALERT_ABOUT_PROTUCT_ADD_TO_BASKET)
     assert "Your basket is empty." in text_value, "Text not present in basket"
