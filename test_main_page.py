d3258f6c12a9a2d22ffde134d279a803931dab35
Tempesto <igor_it_step@gmail.com>
2020-01-31 11:14:22 +0200

    add mark login_guest for test_guest_go_to_login_page and test_guest_should_see_login_link add pytest.ini

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+import pytest
 from .pages.locators.base_page_locators import BasePageLocators
 from .pages.locators.main_page_locator import MainPageLocators
 from .pages.locators.product_page_locator import ProductPageLocator
@@ -10,18 +11,20 @@ link = 'http://selenium1py.pythonanywhere.com/'
 link_product = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
 
 
-def test_guest_go_to_login_page(browser):
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)  # выполняем метод страницы - переходим на страницу логина
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_go_to_login_page(self, browser):
+        page = MainPage(browser,
+                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)  # выполняем метод страницы - переходим на страницу логина
+        login_page.should_be_login_page()
 
-
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    assert page.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        assert page.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
