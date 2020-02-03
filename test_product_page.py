35eda2d95a7f5544e3677d59abcb24d6cd6c3e5f
Tempesto <igor_it_step@gmail.com>
2020-02-03 14:33:46 +0200

    refactoring test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -8,11 +8,9 @@ import time
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('link2', ["0", "1", "2", "3", "4", "5", "6",
-                                   pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
-def test_guest_can_add_product_to_basket(browser, link2):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
-    page = ProductPage(browser, f"{link}{link2}")
+    page = ProductPage(browser, link)
     page.open()
     page.add_product_to_cart()
     page.solve_quiz_and_get_code()
